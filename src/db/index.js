e7d72c2d793565a95c2b7920b047cf3ff426ac72
Aniket <aniketraj28042003@gmail.com>
2024-10-21 23:06:39 +0530

    added

--- src/db/index.js
+++ src/db/index.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async() => {
     try {
-        await mongoose.connect("mongodb+srv://aniketraj28012002:UfeXMYo0gh9HoMrv@cloudproject.ouoim.mongodb.net/?retryWrites=true&w=majority&appName=cloudProject",{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          });
+        await mongoose.connect("mongodb+srv://aniketraj28012002:UfeXMYo0gh9HoMrv@cloudproject.ouoim.mongodb.net/?retryWrites=true&w=majority&appName=cloudProject");
         console.log("db connected successfully");
     } catch (error) {
         console.log(error);
